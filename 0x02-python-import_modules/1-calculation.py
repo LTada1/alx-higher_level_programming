fd3700a99240e98e8ce6ba88292a1e7e0e195002
LTada1 <lukahruna4@gmail.com>
2023-06-14 07:11:10 -0700

    import module

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 
 if __name__ == "__main__":
-	"""print addition of two numbers."""
+	"""print add, sub, mul and div of two numbers 10 and 5."""
 	from calculator_1 import add, sub, mul, div
 
 	a = 10
