f42fe4fabc53c8778a69eeb09bd39dc7f490d37e
Ltada1 <lukaharua4@gmail.com>
2023-08-15 14:31:07 -0700

    style

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
-#1-element_at.py
+# 1-element_at.py
 
 def element_at(my_list, idx):
     if idx < 0:
         return (None)
     elif idx > (len(my_list) - 1):
-        return  (None)
+        return (None)
     return (my_list[idx])
