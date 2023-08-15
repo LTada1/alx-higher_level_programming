4be3e588139c0d8f164657610e8fd8fd8a6bf59d
Ltada1 <lukaharua4@gmail.com>
2023-08-14 20:13:52 -0700

    new

---
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+#4-new_in_list.py
+
+def new_in_list(my_list, idx, element):
+    copy_list = my_list.copy()
+    if idx < 0:
+        return (my_list)
+    if idx > (len(my_list)-1):
+        return (my_list)
+    copy_list[idx] = element
+    return (copy_list)
