13fe652ba863c10121e0dfc3a2ea7050b1eb103d
Ltada1 <lukaharua4@gmail.com>
2023-08-14 20:56:15 -0700

    add

---
+++ 0x03-python-data_structures/5-no_c.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+#5-no_c.py
+
+def no_c(my_string):
+    string = ''
+    for i in my_string:
+        if i == 'c' or i == 'C':
+            continue
+        string += i
+    return (string)
