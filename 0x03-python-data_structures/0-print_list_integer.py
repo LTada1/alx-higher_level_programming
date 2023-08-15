65ebc6cc442bdbff3da56c8bf43b4685197a4feb
Ltada1 <lukaharua4@gmail.com>
2023-08-14 17:05:01 -0700

    Print

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -1,5 +1,8 @@
+#!/usr/bin/python3
+# 0-print_list_integer.py
+
 def print_list_integer(my_list=[]):
     lap = my_list[:]
 
     for i in lap:
-        print("{}".format(i))
+        prinit("{:d}".format(i))
