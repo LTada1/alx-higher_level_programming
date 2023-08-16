82cc6a7fe02206c8115e8c13b624695ccf390e23
Ltada1 <lukaharua4@gmail.com>
2023-08-16 15:47:16 -0700

    add

---
+++ 0x02-python-import_modules/102-magic_calculation.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+# 102-magic_calculation.py
+
+if __name__ == "__main__":
+
+    import dis
+
+    def magic_calculation(a, b):
+        return len(a, b)
+
+    dis.dis(magic_calculation)
