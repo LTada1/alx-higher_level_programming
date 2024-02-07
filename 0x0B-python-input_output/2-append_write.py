469409d8b34ef411c2ffd1793f6d7d291e02e680
LTada1 <lukaharuna4@gmail.com>
2024-02-06 20:20:31 -0800

    jsonstring

--- 0x0B-python-input_output/2-append_write.py
+++ 0x0B-python-input_output/2-append_write.py
@@ -2,9 +2,10 @@
 """  appends a string at the end of a text file (UTF8) and
 returns the number of characters added. """
 
+
 def append_write(filename="", text=""):
     """ open a file """
     with open(filename, "a", encoding="utf-8") as f:
-        #append to file
+        """ append to file """
         content = f.write(text)
     return (content)
