491f2f9c2384bb7631082634c9c7bdaabcd1aa7b
LTada1 <lukaharuna4@gmail.com>
2024-02-06 19:02:15 -0800

    pycodestyle

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 """ Function for reading files"""
 
+
 def read_file(filename=""):
-	""" Reads file content """
-	with open(filename, encoding="utf-8") as f:
-		print(f.read(), end= " ")
+    """ Reads file content """
+    with open(filename, encoding="utf-8") as f:
+        print(f.read(), end="")
